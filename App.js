0428805b9562ca89b7fccddb1ee6f8352890a2af
U-ILINE\ilineproduc <ilineproduc@PRODUCCION.Iline.local>
2019-03-25 08:39:02 +0100

    Mostrando lista de registros, pendiente añadir touchable a cada registro

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, RefreshControl } from "react-native";
 import { createBottomTabNavigator, createStackNavigator, createAppContainer, NavigationEvents } from "react-navigation";
 import axios from 'axios';
-import {ListItem, Header, Button} from 'react-native-elements';
+import {ListItem, Header, Button, Icon} from 'react-native-elements';
 import ScanScreen from "./ScanScreen";
 import PreScanScreen from "./PreScanScreen"
+import { ScrollView } from "react-native-gesture-handler";
 
 
 class HomeScreen extends React.Component {
@@ -12,12 +13,18 @@ class HomeScreen extends React.Component {
   constructor(props){
     super(props)
     this.state = {
-      labellingRecords: []
+      labellingRecords: [],
+      refreshing: false
     }
   }
   
   getLabelRecords = () => {
-      axios.get('http://192.168.0.157:8000/api/labelling').then((labellingRecords) => this.setState({labellingRecords: labellingRecords.data}))
+      axios.get('http://192.168.0.157:8000/api/labelling').then((labellingRecords) => this.setState({labellingRecords: labellingRecords.data, refreshing:false}))
+  }
+
+  _onRefresh = () => {
+    this.setState({refreshing: true});
+    this.getLabelRecords()
   }
   
   componentWillMount(){
@@ -33,7 +40,15 @@ class HomeScreen extends React.Component {
           centerComponent={{ text: 'biltegiApp', style: { color: '#fff' } }}
           rightComponent={{ icon: 'home', color: '#fff' }}/>
 
-        <Button
+      <ScrollView 
+        refreshControl={
+          <RefreshControl
+            refreshing={this.state.refreshing}
+            onRefresh={this._onRefresh}
+          />
+        }>
+
+       {/*  <Button
           icon={{
             name: "arrow-right",
             size: 15,
@@ -41,7 +56,7 @@ class HomeScreen extends React.Component {
           }}
           title="Log records"
           onPress={()=> console.warn(this.state.labellingRecords)}
-        />
+        /> */}
 
 
         {this.state.labellingRecords.map( (record,i) => {
@@ -49,10 +64,28 @@ class HomeScreen extends React.Component {
             <ListItem
               key={i}
               title={record.referencia}
+              leftAvatar={{title: record.operario}}
+              subtitle={
+                <View style={{flex:1, flexDirection: 'row', justifyContent: 'space-between'}}>
+
+                  <View>
+                    <Text>{record.lote}</Text>
+                    <Text>{record.lote_film} - {record.linea}</Text>
+                    <Text></Text>
+                  </View>
+
+                  <View>
+                    <Text>{record.fecha}</Text>
+                    <Text>{record.hora}</Text>
+                  </View>
+
+                </View>
+              }
               topDivider
               bottomDivider
             />
             )})} 
+        </ScrollView>
       </View>
     );
   }
@@ -67,9 +100,25 @@ const PreScanStack = createStackNavigator({
 
 
 const AppNavigator = createBottomTabNavigator({
-  Home: HomeScreen,
-  PreScan:PreScanStack,
-  });
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      tabBarLabel:"Registros",
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="database" size={30} type='foundation' />
+      )
+    }
+  },
+  Scan:
+  {
+    screen: PreScanStack,
+    navigationOptions: {
+      tabBarLabel:"Scan",
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="qrcode-scan" size={30} type='material-community' />
+      )
+    }
+  }})
 
 
 export default createAppContainer(AppNavigator);
